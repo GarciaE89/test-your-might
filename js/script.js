d0591855d68fdd438496d993a0b036f0bd0f7df0
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-08-23 13:08:51 -0600

    added varibles on script file

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var questionsEl = document.querySelector("#questions");
+var timerEl = document.querySelector("#time");
+var optionsEl = document.querySelector("#options");
+var submitBtn = document.querySelector("#submit");
+var startBtn = document.querySelector("#start");
+var initialsEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback");
+
+
