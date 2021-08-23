57d4bf9807a915e731fa52cdd8522061da6ec44d
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-08-23 12:42:48 -0600

    added array of js questions

---
+++ js/javascript-questions.js
@@ -0,0 +1,37 @@
+var jsQuestions = [
+    {
+        title:"What is the correct syntax for creating a variable?",
+        options: ["variable = 5;", "var === 5;","var playerHealth = 100;", "var() = 5;"],
+        answer: "var playerHealth = 100;"
+    },
+    {
+        title:"Which of the following is NOT a Data Type in JavaScript?",
+        options: ["Strings","querySelector", "Numbers","Booleans"],
+        answer: "querySelector"
+    },
+    {
+        title:"What element in HTML is used to wrap JavaScript?",
+        options: ["<script>", "<header>","<style>","<javaScript>"],
+        answer: "<script>"
+    },
+    {
+        title:"alert() is an example of?",
+        options: ["function","variable", "string", "window"],
+        answer: "function"
+    },
+    {
+        title:"what is a callback?",
+        options: ["a function passed, as an arugument to another function", "it displays an alert in the browser window","an HMTL element","a string data type"],
+        answer: "a function passed, as an arugument to another function"
+    },
+    {
+        title: "DOM stands for what?",
+        options : ["Digital Operation Module", "Document Oriented Method", "Digital Object Model", "Document Object Model"],
+        answer: "Document Object Model"
+    },
+    {
+        title: "Where was JavaScript created?",
+        options : ["Apple", "Microsoft", "IBM", "Netscape"],
+        answer: "Netscape"
+    }
+];
