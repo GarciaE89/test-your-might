a6ebf80271c75edee97e43e98e8c2183cb6f0748
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-08-23 13:38:19 -0600

    function to start quiz

--- js/script.js
+++ js/script.js
@@ -7,3 +7,24 @@ var initialsEl = document.querySelector("#initials");
 var feedbackEl = document.querySelector("#feedback");
 
 
+var currentQuestionIndex = 0;
+var time = jsQuestions.length * 15;
+var timerId;
+
+function startQuiz() {
+  
+  var startScreenEl = document.querySelector("#start-screen");
+  startScreenEl.setAttribute("class", "hide");
+
+  
+  questionsEl.removeAttribute("class");
+
+  
+  timerId = setInterval(clockTick, 1000);
+
+  
+  timerEl.textContent = time;
+
+  getQuestion();
+}
+
